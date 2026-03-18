0798c2c69d757bef42d1c0edcdbdb5a81b81324c
Gerarado Navarro <navarrogerardo1409@gmail.com>
2026-03-18 08:42:54 -0600

    feat: registra las rutas de clientes y productos

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\ProductoController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('clientes.index');
 });
+
+Route::resource('clientes', ClienteController::class);
+Route::resource('productos', ProductoController::class);
